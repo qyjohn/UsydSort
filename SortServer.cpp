6c88510524d3f390efa8f926ae22654b7ba41d85
Ubuntu <ubuntu@ip-172-31-19-215.ap-southeast-2.compute.internal>
2016-07-21 21:58:25 +0000

    SortServer version 0.01

--- SortServer.cpp
+++ SortServer.cpp
@@ -15,26 +15,14 @@
 
 using namespace std;
 
-void *task1(void *);
+int create_server(int port);
+void *sort_thread(void *args);
 bool test_exit(char* test);
-string exit_string = "EXIT";
-
-
-template<typename T> void print_queue(T& q) 
-{
-	while(!q.empty()) 
-	{
-		std::cout << q.top();
-		q.pop();
-	}
-	std::cout << '\n';
-}
 
 class SortRecord
 {
 	public:
 	char m_data[100];
-	unsigned int  m_d1, m_d2, m_d3;
 	bool operator<(const SortRecord& other) const
 	{
 
@@ -45,153 +33,176 @@ class SortRecord
 	}
 };
 
-std::ostream& operator<<(std::ostream &strm, const SortRecord &rec) 
+struct thread_args
 {
-	return strm << "SortRecord (" << rec.m_d1 << rec.m_d2 <<rec.m_d3 << ")\n";
-}
+	int socket;
+	std::priority_queue<SortRecord> *queue;
+};
 
-
-static int connFd;
 
 int main(int argc, char* argv[])
 {
-    int pId, portNo, listenFd, totalThread;
-    socklen_t len; //store size of the address
-    bool loop = false;
-    struct sockaddr_in svrAdd, clntAdd;
-    std::priority_queue<int> q;
-    
-    if (argc < 3)
-    {
-        cerr << "Syntam : ./server <port> <total_threads>" << endl;
-        return 0;
-    }
-    
-    portNo = atoi(argv[1]);
-    totalThread = atoi(argv[2]);
-    pthread_t threadA[totalThread];
-    
-    if((portNo > 65535) || (portNo < 2000))
-    {
-        cerr << "Please enter a port number between 2000 - 65535" << endl;
-        return 0;
-    }
-    
-    //create socket
-    listenFd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(listenFd < 0)
-    {
-        cerr << "Cannot open socket" << endl;
-        return 0;
-    }
-    
-    bzero((char*) &svrAdd, sizeof(svrAdd));
-    
-    svrAdd.sin_family = AF_INET;
-    svrAdd.sin_addr.s_addr = INADDR_ANY;
-    svrAdd.sin_port = htons(portNo);
-    
-    //bind socket
-    if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
-    {
-        cerr << "Cannot bind" << endl;
-        return 0;
-    }
-    
-    listen(listenFd, 5);
-    
-    len = sizeof(clntAdd);
-    
-    int noThread = 0;
-	int clientSocket;
-    while (noThread < totalThread)
-    {
-        cout << "Listening" << endl;
-
-        //this is where client connects. svr will hang in this mode until client conn
-        clientSocket = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
-
-        if (clientSocket < 0)
-        {
-            cerr << "Cannot accept connection" << endl;
-            return 0;
-        }
-        else
-        {
-            cout << "Connection successful" << endl;
-        }
-        
-//        pthread_create(&threadA[noThread], NULL, task1, NULL); 
-	pthread_create(&threadA[noThread], NULL, task1, (void*) &clientSocket); 
-        noThread++;
-    }
-    
-    for(int i = 0; i < totalThread; i++)
-    {
-        pthread_join(threadA[i], NULL);
-    }
-    
-    
-}
-
-
-
-//void *task1 (void *dummyPt)
-void *task1 (void *clientSocket)
-{
-	int sock = *(int*) clientSocket;
-
-    cout << "Thread No: " << pthread_self() << endl;
-    char buffer[100], temp[100];
-    bzero(buffer, 101);
-    bzero(temp, 101);
-    
-    int i, j, size;
-    int marker = 0;
-	std::priority_queue<SortRecord> q;
-    
-	
-    while(true)
-    {
-        bzero(buffer, 101);
-	size=recv(sock, buffer, 100, 0);
-
-	for (i=0; i<size; i++)
+	// Check command syntax
+	if (argc < 3)
 	{
-		temp[marker] = buffer[i];
-		marker++;
-		if (marker == 100)	// 100 bytes per record
-		{
-			// Create a SortRecord
-			SortRecord sr;
-			for (j=0; j<100; j++)	sr.m_data[j] = temp[j];
-			q.push(sr);
-
-			// Start another new record
-			marker = 0;
-		}
-
+		cerr << "Syntax : ./SortServer <port> <total_threads>" << endl;
+		return 0;
 	}
-	
-	// Test if the client disconnects
-	if ((marker == 4) && test_exit(temp))	break;
-    }
-
-    cout << "\nClosing thread and conn" << endl;
-    close(sock);
-
-	// Save to file
-	std::ofstream outfile ("new.txt",std::ofstream::binary);
-	while(!q.empty()) 
+    
+	// Create the server socket
+	int portNo = atoi(argv[1]);
+	int listenFd = create_server(portNo);
 	{
-		outfile.write (q.top().m_data, 100);
-		q.pop();
+		if (listenFd <=0)	exit(1);
+	}
+
+	// Wait for all Sender connections
+	// For each Sender, create a pthread with a dedicated queue to handle the incoming data
+	int noThread = 0;
+	int totalThread = atoi(argv[2]);
+	struct sockaddr_in clntAdd;
+	socklen_t len = sizeof(clntAdd);	
+	pthread_t threadA[totalThread];
+	std::priority_queue<SortRecord> queues[totalThread];
+	while (noThread < totalThread)
+	{
+		//this is where client connects. svr will hang in this mode until client conn
+		int clientSocket = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
+		if (clientSocket < 0)
+		{
+			cerr << "Cannot accept connection" << endl;
+			return 0;
+		}
+        
+		// Create a new thread to handle the input
+		struct thread_args	myArgs;
+		myArgs.socket = clientSocket;
+		myArgs.queue = &queues[noThread];		
+		pthread_create(&threadA[noThread], NULL, sort_thread, (void*) &myArgs); 
+		noThread++;
+	}
+    
+	// Wait for all theads to exit
+	for(int i = 0; i < totalThread; i++)
+	{
+		pthread_join(threadA[i], NULL);
+	}
+    
+	// Merge all queues
+	for(int i = 1; i < totalThread; i++)
+	{
+		while(!queues[i].empty()) 
+		{
+			queues[0].push(queues[i].top());
+			queues[i].pop();
+		}    
+	}
+    
+	// Save sorted results to file
+	std::ofstream outfile ("new2.txt",std::ofstream::binary);
+	while(!queues[0].empty()) 
+	{
+		outfile.write (queues[0].top().m_data, 100);
+		queues[0].pop();
 	}
 	outfile.close();
-
 }
 
+/**
+ *
+ * Create the server socket to accept incoming Sender connections
+ *
+ */
+
+int create_server(int port)
+{
+	// check port range
+	if((port > 65535) || (port < 2000))
+	{
+		cerr << "Please enter a port number between 2000 - 65535" << endl;
+		return 0;
+	}
+	    
+	//create socket
+	int listenFd = socket(AF_INET, SOCK_STREAM, 0);	    
+	if(listenFd < 0)
+	{
+		cerr << "Cannot open socket" << endl;
+		return 0;
+	}
+	    
+	// initialization
+	struct sockaddr_in svrAdd;
+	bzero((char*) &svrAdd, sizeof(svrAdd));	    
+	svrAdd.sin_family = AF_INET;
+	svrAdd.sin_addr.s_addr = INADDR_ANY;
+	svrAdd.sin_port = htons(port);
+	    
+	//bind socket
+	if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
+	{
+		cerr << "Cannot bind" << endl;
+		return 0;
+	}
+	    
+	// listen on socket
+	listen(listenFd, 5);
+	return listenFd;
+}
+
+/**
+ *
+ * A sorting thread, handling the data from one particular Sender.
+ * The incoming data is pushed into a std::priority_queue.
+ *
+ */
+
+void *sort_thread (void *args)
+{
+	struct thread_args *myArgs = (struct thread_args*) args;
+	int sock = myArgs->socket;	// the socket
+	std::priority_queue<SortRecord> *q = myArgs->queue;	// the queue
+
+	// Print out debug message
+	cout << "Thread No: " << pthread_self() << endl;
+	char buffer[100], temp[100];
+    
+	int i=0, j=0, size=0, marker=0;
+	while(true)
+	{
+		// Attempt to receive 100 bytes each time
+		size=recv(sock, buffer, 100, 0);
+		for (i=0; i<size; i++)
+		{
+			temp[marker] = buffer[i];
+			marker++;
+			if (marker == 100)	// 100 bytes per record
+			{
+				// Create a SortRecord
+				SortRecord sr;
+				for (j=0; j<100; j++)	sr.m_data[j] = temp[j];
+				q->push(sr);
+
+				// Start another new record
+				marker = 0;
+			}
+		}
+	
+		// Test if the client disconnects
+		if ((marker == 4) && test_exit(temp))	break;
+	}
+
+	// Print out debug message
+	cout << "\nExciting thread " << pthread_self() << endl;
+	close(sock);
+}
+
+
+/**
+ *
+ * Checking the EXIT signal from a Sender.
+ *
+ */
 
 bool test_exit(char* test)
 {
